510769941018629f8c1137025be908187ddbc995
kush Muhamad <kushmohammed98@yahoo.com>
2023-06-29 12:30:53 +0300

    completed task

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,6 +1,12 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
+      t.string :brand
+      t.integer :capacity
+      t.string :number_plate
+
+      
+
 
       t.timestamps
     end
